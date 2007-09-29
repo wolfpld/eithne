d52cb5756ae7db6768a6d6f99af1919c21d65124
Bartosz Taudul <wolf.pld@gmail.com>
2007-09-30 01:06:47 +0200

    - new plugin: histogram view

---
+++ Plugins/HistogramView/HistogramView.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Xml;
+using Mono.Unix;
+
+namespace Eithne
+{
+	public class HistogramViewInfo : IInfo
+	{
+		public override string Name
+		{
+			get { return Catalog.GetString("Histogram View"); }
+		}
+
+		public override string ShortName
+		{
+			get { return "HistView"; }
+		}
+
+		public override string Author
+		{
+			get { return "Bartosz Taudul"; }
+		}
+
+		public override string Description
+		{
+			get { return Catalog.GetString("This plugin displays histogram graphs."); }
+		}
+	}
+
+	public class HistogramViewFactory : IFactory
+	{
+		IInfo _info = new HistogramViewInfo();
+		public IInfo Info
+		{
+			get { return _info; }
+		}
+
+		public IType Type
+		{
+			get { return IType.Out; }
+		}
+
+		public void Initialize()
+		{
+		}
+
+		public IPlugin Create()
+		{
+			return new HistogramViewPlugin();
+		}
+	}
+
+	public class HistogramViewPlugin : IOutPlugin
+	{
+		Gdk.Pixbuf[] images = null;
+		Gdk.Pixbuf[] thumbs = null;
+		int[] cat = null;
+
+		public HistogramViewPlugin()
+		{
+			_info = new HistogramViewInfo();
+		}
+
+		public override void DisplayResults()
+		{
+			if(!_workdone)
+				throw new PluginException(Catalog.GetString("Plugin is not ready to display images."));
+
+			new HistogramViewWindow(images, thumbs, cat);
+		}
+
+		public override void Work()
+		{
+			ICommImage socket = (ICommImage)_in[0];
+			IImage[] img = socket.Images;
+			IImage[] orig = socket.OriginalImages;
+
+			images = new Gdk.Pixbuf[img.Length];
+			thumbs = new Gdk.Pixbuf[img.Length];
+			cat = socket.Categories;
+
+			double scale;
+
+			for(int i=0; i<img.Length; i++)
+			{
+				IImage _img = new IImage(orig[i].BPP, orig[i].W, orig[i].H, orig[i].Data);
+
+				images[i] = HistogramGraph(img[i]);
+
+				Gdk.Pixbuf pixbuf = Utility.CreatePixbuf(_img);
+
+				if(_img.W > _img.H)
+					scale = _img.W / 64.0;
+				else
+					scale = _img.H / 64.0;
+
+				thumbs[i] = pixbuf.ScaleSimple(Scale(_img.W, scale), Scale(_img.H, scale), Gdk.InterpType.Bilinear);
+			}
+
+			_workdone = true;
+		}
+
+		private int Scale(int s, double scale)
+		{
+			int val = (int)(s/scale);
+
+			if(val == 0)
+				return 1;
+			else
+				return val;
+		}
+
+		private Gdk.Pixbuf HistogramGraph(IImage img)
+		{
+			IImage graph = new IImage(1, img.W, 256);
+
+			graph.Clear((byte)255);
+			
+			for(int i=0; i<img.W; i++)
+			{
+				int hval = (byte)img[i, 0];
+				
+				for(int j=0; j<hval; j++)
+				{
+					graph[i, 255-j] = (byte)0;
+				}
+			}
+			
+			return Utility.CreatePixbuf(graph);
+		}
+		
+		public override int NumIn		{ get { return 1; } }
+
+		public override string DescIn(int n)
+		{
+			return Catalog.GetString("Histograms to be viewed.");
+		}
+
+		private static string[] matchin   = new string[] { "image/grayscale/histogram" };
+		public override string[] MatchIn	{ get { return matchin; } }
+	}
+}
---
+++ Plugins/HistogramView/HistogramViewWindow.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reflection;
+using Gtk;
+using Glade;
+using Mono.Unix;
+
+namespace Eithne
+{
+	public class ImageButton : Button
+	{
+		private int n;
+
+		public ImageButton(Gtk.Widget w, int n) : base(w)
+		{
+			this.n = n;
+		}
+		
+		public int Num
+		{
+			get { return n; }
+		}
+	}
+
+	public class HistogramViewWindow
+	{
+		[Widget] Window		HVWindow;
+		[Widget] Button		CloseButton;
+		[Widget] Label		CounterText;
+		[Widget] Label		ImageCategory;
+		[Widget] Image		CurrentImage;
+		[Widget] ScrolledWindow	ImageListSocket;
+
+		private Gdk.Pixbuf[] img;
+		private int[] cat;
+
+		public HistogramViewWindow(Gdk.Pixbuf[] img, Gdk.Pixbuf[] thumbs, int[] cat)
+		{
+			this.img = img;
+			this.cat = cat;
+
+			Glade.XML gxml = new Glade.XML(Assembly.GetExecutingAssembly(), "HistogramView.glade", "HVWindow", null);
+			gxml.BindFields(this);
+
+			HVWindow.DeleteEvent += CloseWindow;
+			CloseButton.Clicked += CloseWindow;
+
+			CounterText.Text = String.Format(Catalog.GetPluralString("<i>Viewing {0} image histogram</i>", "<i>Viewing {0} image histograms</i>", img.Length), img.Length);
+			CounterText.UseMarkup = true;
+
+			ShowImage(0);
+
+			HButtonBox hb = new HButtonBox();
+
+			hb.Layout = ButtonBoxStyle.Start;
+			for(int i=0; i<thumbs.Length; i++)
+			{
+				ImageButton b = new ImageButton(new Image(thumbs[i]), i);
+				b.Clicked += OnClicked;
+				hb.PackEnd(b, false, false, 0);
+			}
+
+			ImageListSocket.AddWithViewport(hb);
+
+			HVWindow.ShowAll();
+		}
+
+		private void ShowImage(int n)
+		{
+			CurrentImage.FromPixbuf = img[n];
+			ImageCategory.Text = String.Format(Catalog.GetString("Image category: {0}"), cat[n]);
+		}
+
+		private void OnClicked(object o, EventArgs args)
+		{
+			ShowImage((o as ImageButton).Num);
+		}
+
+		private void CloseWindow(object o, EventArgs args)
+		{
+			img = null;
+			cat = null;
+			HVWindow.Destroy();
+		}
+	}
+}
